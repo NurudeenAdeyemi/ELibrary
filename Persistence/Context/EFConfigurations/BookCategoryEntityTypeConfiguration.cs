97459567cd0a28cb63a1ea12265e5a933c7f7970
adebisi4145 <adeyemiadebisi00@yahoo.com>
2021-10-13 16:46:54 +0100

    EfConfigurations

---
+++ Persistence/Context/EFConfigurations/BookCategoryEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context.EFConfigurations
+{
+    public class BookCategoryEntityTypeConfiguration : IEntityTypeConfiguration<BookCategory>
+    {
+        public void Configure(EntityTypeBuilder<BookCategory> builder)
+        {
+            builder.ToTable("BookCategories");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.BookId)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(u => u.CategoryId)
+               .HasColumnType("int")
+               .IsRequired();
+        }
+    }
+}
---
+++ Persistence/Context/EFConfigurations/UserRoleEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context.EFConfigurations
+{
+    public class UserRoleEntityTypeConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.ToTable("BookRole");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.UserId)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(u => u.RoleId)
+               .HasColumnType("int")
+               .IsRequired();
+        }
+    }
+}
